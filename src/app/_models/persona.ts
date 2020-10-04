595372c4dc60b17d64c2c81b0b91f714311d0359
Raúl González <gonzalv@ciencias.unam.mx>
2020-10-04 13:33:22 -0500

    [MODIFIED] Estilo de la vista

--- src/app/_models/persona.ts
+++ src/app/_models/persona.ts
@@ -10,8 +10,22 @@ export class Persona {
         this.id = id;
         this.nombre = nombre;
         this.apellidos = apellidos;
-        this.fecha_nacimiento = fecha_nacimiento;
+        this.fecha_nacimiento = new Date(fecha_nacimiento);
         this.domicilio = domicilio;
         this.rfc = rfc;
+    }
+
+    getFullName() {
+        return `${this.nombre} ${this.apellidos}`
+    }
+
+    getFormatBirthDate() {
+        var months:String[] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        var days:String[] = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        var diaNombre:String = days[this.fecha_nacimiento.getDay()];
+        var dia:Number = this.fecha_nacimiento.getDate();
+        var mes:String = months[this.fecha_nacimiento.getMonth()];
+        var year:Number = this.fecha_nacimiento.getFullYear();
+        return `${diaNombre} ${dia} de ${mes} de ${year}`;
     }
 }
